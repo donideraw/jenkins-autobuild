f135e5f7ad100dd068c27232a295da9ff5c33fd7
donideraw <donideraw@gmail.com>
2022-05-31 10:05:23 +0700

    init project

---
+++ src/test/java/com/example/jenkinsbuild/JenkinsBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
