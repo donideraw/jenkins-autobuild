21b881801639e3b6bebf365369ab56db7694fef3
donideraw <donideraw@gmail.com>
2022-05-31 10:42:54 +0700

    add path variable

---
+++ src/main/java/com/example/jenkinsbuild/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsbuild;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<String> hello(@PathVariable(name = "name") String name) {
+        return ResponseEntity.ok("Halo " + name);
+    }
+
+}
